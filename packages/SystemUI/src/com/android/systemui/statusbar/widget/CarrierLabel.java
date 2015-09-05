099500eafc13bdf5ff4ec659d5d273ccaee7ee24
winga45 <Winga45@gmail.com>
2015-09-05 13:56:35 +0000

    Revert "FWB: Status bar carrier label font size (2/2)"
    This reverts commit fdb1fda80d1b4fa39603bc3a96ad260cda78f40c.

--- packages/SystemUI/src/com/android/systemui/statusbar/widget/CarrierLabel.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/widget/CarrierLabel.java
@@ -47,7 +47,6 @@ public class CarrierLabel extends TextView {
     private Context mContext;
     private boolean mAttached;
     private static boolean isCN;
-    private int mCarrierFontSize = 14;
 
     protected int mCarrierColor = getResources().getColor(R.color.kg_carrier_text);
     Handler mHandler;
@@ -61,14 +60,11 @@ public class CarrierLabel extends TextView {
             ContentResolver resolver = mContext.getContentResolver();
             resolver.registerContentObserver(Settings.System
                     .getUriFor(Settings.System.STATUS_BAR_CARRIER_COLOR), false, this);
-            resolver.registerContentObserver(Settings.System
-                    .getUriFor(Settings.System.STATUS_BAR_CARRIER_FONT_SIZE), false, this);
         }
 
         @Override
         public void onChange(boolean selfChange) {
             updateColor();
-            updateSize();
         }
     }
 
@@ -88,7 +84,6 @@ public class CarrierLabel extends TextView {
         SettingsObserver settingsObserver = new SettingsObserver(mHandler);
         settingsObserver.observe();
         updateColor();
-        updateSize();
     }
 
     @Override
@@ -179,12 +174,5 @@ public class CarrierLabel extends TextView {
              mCarrierColor = defaultColor;
         }
         setTextColor(mCarrierColor);
-    }
-
-    private void updateSize() {
-        mCarrierFontSize = Settings.System.getInt(mContext.getContentResolver(),
-                Settings.System.STATUS_BAR_CARRIER_FONT_SIZE, 14);
-
-        setTextSize(mCarrierFontSize);
     }
 }
